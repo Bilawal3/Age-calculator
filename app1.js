7c491385f4ced18ba6e69e4641ac6efaddf12a87
bilawal <bilawalsoomro616@gmail.com>
2024-05-06 23:39:31 +0500

    agecalculator

--- app1.js
+++ app1.js
@@ -4,8 +4,7 @@ function calculatorAge(){
     var today = new Date();
     var age = today.getFullYear() - dobDate.getFullYear();
     var monthDiff = today.getMonth() - dobDate.getMonth();
-    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dobDate.getDate())) {
-        age--;
-    }
-    document.getElementById('result').innerText = "Your age is : " + age;
+    var day = today.getDate() - dobDate.getDate();  
+
+    document.getElementById('result').innerText = `Your age is : ${age} year ${monthDiff} mouth ${day} days`
 }
